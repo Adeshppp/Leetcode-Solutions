1b89540efbda7145f97962f4c69ab488e30aece8
Adesh <adeshp948@gmail.com>
2022-11-13 08:51:34 -0500

    Updated better solution

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,31 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class GroupAnagrams {
+class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> anagrams=new ArrayList<List<String>>();
-        for(int i=0;i<strs.length;i++){
-            if(strs[i]==null) continue;
-            List<String> anagramsGroup=new ArrayList<String>();
-            anagramsGroup.add(strs[i]);
-            for(int j=i+1;j<strs.length;j++){
-                if(strs[j]==null) continue;
-                if(isAnagram(strs[i],strs[j])){
-                    anagramsGroup.add(strs[j]);
-                    strs[j]=null;
-                }
-            }
-            anagrams.add(anagramsGroup);
+        if(strs==null || strs.length==0) return new ArrayList<>();
+        Map<String, ArrayList<String>> map=new HashMap<>();
+        for(String str: strs){
+            char[] c=str.toCharArray();
+            Arrays.sort(c);
+            String s=String.valueOf(c);
+            if(!map.containsKey(s)) map.put(s,new ArrayList<>());
+            map.get(s).add(str);
         }
-        return anagrams;
+        return new ArrayList<>(map.values());
 
-    }
-    public  boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        int[] a=new int[26];
-        for(int i=0;i<s.length();i++) a[s.charAt(i) - 'a']++;
-        for(int i=0;i<t.length();i++) a[t.charAt(i) - 'a']--;
-        for(int i:a) if (i!=0) return false;
-        return true;
     }
 }
