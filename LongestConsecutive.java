c8a10facb3feebd5d22f7f2b1b41bad50cc72462
Adesh <adeshp948@gmail.com>
2022-12-09 17:42:08 -0500

    Logestconsecutive solution. It is more optimal and less complex solution than previous commits.

--- LongestConsecutive.java
+++ LongestConsecutive.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutive {
     public static void main(String[] args) {
@@ -8,28 +9,18 @@ public class LongestConsecutive {
         System.out.println(longestConsecutive(arr));
     }
 
-    public static int longestConsecutive(int[] nums){
-        HashSet<Integer> set=new HashSet<>();
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set=new HashSet<>();
         for(int i:nums) set.add(i);
-        ArrayList<Integer>[] al=new ArrayList[nums.length]; //declaring arraylist in arraylist type array
-        for(int i=0;i<nums.length;i++) al[i]=new ArrayList<Integer>();
-        int index=0;
+        int z=0;
+        int c=0;
         for (int num : nums) {
-            if (set.contains(num - 1) || set.contains(num + 1)) {
-                if (set.contains(num - 1)) al[index].add(num - 1);
-                if (set.contains(num + 1)) al[index].add(num + 1);
-                al[index].add(num);
-            } else index++;
+            int count = 0;
+            if (set.contains(num - 1)) continue;
+            while (set.contains(num + z++)) count++;
+            z = 0;
+            if (count > c) c = count;
         }
-        set.clear();
-        //removing duplicates
-        for(ArrayList<Integer> list:al){
-            set= new HashSet<>(list);
-            list.clear();
-            list.addAll(set);
-        }
-        int size=0;
-        for(ArrayList<Integer> list:al) if(size<list.size()) size=list.size();
-        return size;
+        return c;
     }
 }
